c8120554dfb6b03fc2184659d1798a594840aeda
david-codeatelier <david@codeatelier.com>
2024-10-28 18:08:54 +0100

    Format

--- packages/svelte-form/vite.config.ts
+++ packages/svelte-form/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import packageJson from './package.json'
 
 export default defineConfig({
-    plugins: [svelte()],
+  plugins: [svelte()],
 
-    test: {
-        name: packageJson.name,
-        dir: './tests',
-        watch: false,
-        environment: 'jsdom',
-        setupFiles: ['./tests/test-setup.ts'],
-        coverage: {
-          // There seems to be some svelte-5 incompatibility so this is disabled for now
-          enabled: false,
-          provider: 'istanbul',
-          include: ['src/**/*'],
-        },
-        typecheck: { enabled: true },
-      },
-});
+  test: {
+    name: packageJson.name,
+    dir: './tests',
+    watch: false,
+    environment: 'jsdom',
+    setupFiles: ['./tests/test-setup.ts'],
+    coverage: {
+      // There seems to be some svelte-5 incompatibility so this is disabled for now
+      enabled: false,
+      provider: 'istanbul',
+      include: ['src/**/*'],
+    },
+    typecheck: { enabled: true },
+  },
+})
